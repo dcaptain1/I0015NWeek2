84e63e0c781a1236409a261ee72e58940dfd464d
oscarthern <oscar.thern@gmail.com>
2022-04-05 11:16:45 +0100

    hej

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,15 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World, version 11"); //detta är en kommentar
+        System.out.println("Hello World, version 11");
+        System.out.println("HEJ!");
+        //detta är en kommentar
     }
+
     // kommentar 2
     // och 3
 
+}
     // En till kommentar.
 
-}
+
