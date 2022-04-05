1191f2ffcbcdd9f2d57d67283a81471111cffa8c
emierr <emierr-1@student.ltu.se>
2022-04-05 10:30:32 +0200

    kommentar

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World, version 11");
+        System.out.println("Hello World, version 11"); //detta är en kommentar
     }
 
 
